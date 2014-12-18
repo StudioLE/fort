fc6b1cb81b3ba16280f3b5e3d43b4078453fe685
Laurence Elsdon <git@elsdon.me>
2014-12-18 17:12:18 +0000

    TV Show aliases

--- config.js
+++ config.js
@@ -1,11 +1,25 @@
 module.exports = {
 
+	// TV Show directory (or drive)
 	tv_directory: 'M:\\TV',
 
+	// Movie directory (or drive)
 	movies_directory: 'M:\\Movies',
 
+	// Download directory (or drive)
 	download_directory: 'C:\\Users\\LE\\Downloads\\FTP',
 
-	ignored_keyword: [ 'the', 'and', 'an', 'a', 'in', 'of' ]
+	// Episodes with titles matching 'title' will be renamed to 'change_to'
+	// Use lower case for 'title'
+	show_aliases: [
+		{
+			title: 'castle',
+			change_to: 'Castle 2009'
+		}
+	]
+
+	// Keywords that will be ignored when matching titles
+	// @todo implement feature?
+	//ignored_keyword: [ 'the', 'and', 'an', 'a', 'in', 'of' ]
 
 }
