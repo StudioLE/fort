8a75c8de6fd3d052bbe9aa0a32d73b284f3ab822
Laurence Elsdon <git@elsdon.me>
2014-12-18 13:12:36 +0000

    Reformatted console output

--- fort.js
+++ fort.js
@@ -9,6 +9,23 @@ var prep_downloads = require('./lib/prep_downloads')
 // Config 
 var config = require('./config')
 
+
+// --------------------------------------------------
+// Fort - Fetch and sort for media archives
+// --------------------------------------------------
+
+var out = []
+var hr = chalk.gray('--------------------------------------------------')
+
+// Format the console output
+out.push(hr)
+out.push(chalk.cyan.bold('Fort ') + chalk.gray('- Fetch and sort for media archives'))
+out.push(hr)
+out.push(chalk.magenta('Download directory: ') + chalk.gray(config.download_directory))
+out.push(chalk.magenta('TV directory: ') + chalk.gray(config.tv_directory))
+out.push(chalk.magenta('Movies directory: ') + chalk.gray(config.movies_directory))
+out.push(hr)
+
 // Begin script
 async.waterfall([
 
@@ -18,11 +35,27 @@ async.waterfall([
 			callback(err, episodes, ignored)
 		})
 
-	}, 
+	},
 
 	function(episodes, ignored, callback) {
 
 		prep_downloads(episodes, function(err, copy, extract) {
+
+			// Copy
+			for(var i in copy) {
+				out.push(chalk.green('Copy: ') + chalk.gray(copy[i].copy))
+			}
+
+			// Extract
+			for(var i in extract) {
+				out.push(chalk.cyan('Extract: ') + chalk.gray(extract[i].extract))
+			}
+
+			// Ignore
+			for(var i in ignored) {
+				out.push(chalk.yellow('Ignore: ') + chalk.gray(ignored[i]))
+			}
+			
 			callback(err, copy, extract, ignored)
 		})
 
@@ -32,32 +65,6 @@ async.waterfall([
 function(err, copy, extract, ignored) {
 
 	if(err) throw err;
-
-	var out = []
-	var hr = chalk.gray('------------------------------------------------')
-
-	out.push(hr)
-	out.push(chalk.cyan.bold('Fort ') + chalk.gray('- Fetch and sort for media archives'))
-	out.push(hr)
-	out.push(chalk.blue('Download directory: ') + chalk.gray(config.download_directory))
-	out.push(chalk.blue('TV directory: ') + chalk.gray(config.tv_directory))
-	out.push(chalk.blue('Movies directory: ') + chalk.gray(config.movies_directory))
-	out.push(hr)
-	
-	// Copy
-	for(var i in copy) {
-		out.push(chalk.green('Copy: ') + chalk.gray(copy[i].copy))
-	}
-
-	// Extract
-	for(var i in extract) {
-		out.push(chalk.magenta('Extract: ') + chalk.gray(extract[i].extract))
-	}
-
-	// Ignore
-	for(var i in ignored) {
-		out.push(chalk.yellow('Ignore: ') + chalk.gray(ignored[i]))
-	}
 
 	// Log the output
 	for(var i in out) console.log(out[i])
