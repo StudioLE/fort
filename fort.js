3be65b8ed80e1858d1bbe8831e7cc0157a662104
Laurence Elsdon <git@elsdon.me>
2015-03-18 13:15:34 +0000

    Modularised code

--- fort.js
+++ fort.js
@@ -1,18 +1,11 @@
 // Node Modules 
-var async = require('async')
-var chalk = require('chalk')
 var sqwk = require('sqwk')
 
 // Fort Modules
-//var sqwk.= require('./lib/sqwk.)
-var read_downloads = require('./lib/read_downloads')
-var read_tv = require('./lib/read_tv')
-var match_show = require('./lib/match_show')
-var prep_downloads = require('./lib/prep_downloads')
-var move_downloads = require('./lib/move_downloads')
-var extract_downloads = require('./lib/extract_downloads')
+var sort_downloads = require('./lib/sort_downloads')
+var scan_tv = require('./lib/scan_tv')
 
-// Config 
+// Config
 var config = require('./config')
 
 
@@ -31,83 +24,21 @@ sqwk.send([
 	'{hr}',
 ])
 
-// Begin script
-async.waterfall([
+// Read the 3rd command line argument
+var operation = process.argv[2]
 
-	// Read the download directory
-	function(callback) {
+if(operation == 'sort') {
+	sort_downloads()
+}
+else if(operation == 'fetch') {
+	console.log('Fetch')
 
-		read_downloads(config.download_directory, function(err, episodes, ignored) {
-			callback(err, episodes, ignored)
-		})
-
-	},
-
-	// Match downloads to TV shows
-	function(episodes, ignored, callback) {
-		
-		read_tv(config.tv_directory, function(err, shows) {
-			match_show(shows, episodes, ignored, function(error, episodes) {
-				callback(err, episodes, ignored)
-			})
-		})
-
-	},
-
-	// Decide what needs to be copied or extracted
-	function(episodes, ignored, callback) {
-
-		prep_downloads(episodes, ignored, function(err, copy, extract, ignored) {
-
-			if(config.log_intent) {
-
-				// Copy
-				for(var i in copy) {
-					sqwk.send([['{green}', 'Copy:', copy[i].episode_title]])
-					//if(config.detailed_information) console.log(copy[i])
-				}
-
-				// Extract
-				for(var i in extract) {
-					sqwk.send([['{cyan}', 'Extract:', extract[i].episode_title]])
-					//if(config.detailed_information) console.log(extract[i])
-				}
-
-				// Ignore
-				for(var i in ignored) {
-					sqwk.send([['{yellow}', 'Ignore:', ignored[i].file_name + ' (' + ignored[i].ignored + ')']])
-					if(config.detailed_information) console.log(ignored[i])
-				}
-
-				sqwk.send(sqwk.hr())
-			}
-
-			callback(err, copy, extract, ignored)
-		})
-
-	},
-
-	// Move episodes
-	function(copy, extract, ignored, callback) {
-
-		move_downloads(copy, function(err) {
-			callback(err, extract, ignored)
-		})
-
-	},
-
-	// Extract episodes
-	function(extract, ignored, callback) {
-
-		extract_downloads(extract, function(err) {
-			callback(err, ignored)
-		})
-	}
-
-],
-function(err, ignored) {
-
-	if(err) throw err
-
-	sqwk.send()
-})
+}
+else if(operation == 'scan') {
+	scan_tv(function(err, list) {
+		console.log(list)
+	})
+}
+else {
+	console.log('invalid operation %s', operation)
+}
